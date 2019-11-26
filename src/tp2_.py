3d55f978ba7cc71281df01dff86acf91ff6b4bd5
klascolin <me.nicolas7@gmail.com>
2019-11-26 03:57:30 -0300

    agrego version tp2_ que hace los downsamples para que funcione en mi compu y que esta un poco mas prolija

--- src/tp2_.py
+++ src/tp2_.py
@@ -4,6 +4,7 @@ from pynput import mouse
 import recorder
 import signal, os
 import sox   
+import pandas as pd
 #import simpleaudio as sa
 #from playsound import playsound
 #import simpleaudio.functionchecks as fc
@@ -25,14 +26,12 @@ pid = -1
 process=None
 proc=None
 
-
 class Estados(Enum):
   Escuchando = 0
   Reproduciendo = 1
   Pausado = 2
 
 estado = Estados.Escuchando
-
 
 #la reproduccion con esta funcion se escucha un poco raro, hasta ahora es la unica alternativa que me funciono "bien"
 def play_wav(wav_filename, chunk_size=1024):
@@ -85,6 +84,12 @@ def resToString(res):
   else:
     return ''
 
+def resample(rate,file):
+  tfm = sox.Transformer()
+  tfm.convert(samplerate=rate, n_channels=1,bitdepth=16)
+  tfm.build(file+".wav", file+"_final.wav")
+  return
+
 def listar(band_filter=None):
   print(band_filter)
   df = pd.read_csv('canciones/songs.csv',header = 0)
@@ -103,15 +108,11 @@ def listar(band_filter=None):
 
 def procesar(audioName = 'audio_final.wav'):
   global pid, estado, Estados, process, proc
-  cmd, res = sp.reconocer(audioName)
-  #str1 = ''.join(res)
-
-  print(cmd)
-  print(res)
-  str1 = resToString(res)
-  print(str1)
   msg = ""
   song=""
+  cmd, res = sp.reconocer(audioName)
+  str1 = resToString(res)
+  print(cmd)
   
   if (re.search("poner|reproducir|poné", str1)):
     encontro = True
@@ -150,22 +151,15 @@ def procesar(audioName = 'audio_final.wav'):
       encontro = False
 
     if encontro == True:
-      #pid = os.fork()
-      #if(pid == 0):
       if process != None:
         process.terminate()
-        #process.close()
+      
       process = Process(target=play_wav, args=('canciones/'+song, 1024))
       process.start()
       pid = process.pid
       proc = psutil.Process(pid)
-
       print("Encontró:")
-      #msg = "Reproduciendo"
-        #play_wav('data/songs/el_revelde.wav')
-        #os._exit(os.EX_OK)
-      #else:
-      #  print(pid)
+      
     else:
       print("No Encontró:")
       msg = "No se encontró lo buscado"
@@ -176,7 +170,6 @@ def procesar(audioName = 'audio_final.wav'):
       estado = Estados.Pausado
       if process != None:
         proc.suspend()
-      #os.kill(pid, signal.SIGSTOP)
     else:
       print("No se puede pausar porque no está reproduciendo...")
       msg = "No se puede pausar porque no está reproduciendo"
@@ -187,21 +180,24 @@ def procesar(audioName = 'audio_final.wav'):
       estado = Estados.Reproduciendo
       if process != None:
         proc.resume()
-      #os.kill(pid, signal.SIGCONT)
     else:
       print("No se puede reanudar porque no está pausado...")
       msg = "No se puede reanudar porque no está pausado"
+  elif( re.search("listar|listá",str1)):
+    band = None
+    match = re.search("queen|la renga|los redondos|bon jovi",str1)
+    if(match):
+      band = match[0]
+    listar(band)
   else:
     print("No reconoce comando...")
     msg = "No reconoce comando"
 
+  print(msg)
   sp.sintetizar('tmp/salida.wav',msg)
-  #if process != None:
-  #  process.terminate()
-  #  process.close()
-  #process = Process(target=play_wav, args=('tmp/salida.wav'))
-  #process.start()
-  play_wav('tmp/salida.wav')
+  resample(48000,'tmp/salida')  
+  play_wav('tmp/salida_final.wav')
+
   return
 
 def on_click(x, y, button, pressed):
@@ -211,32 +207,22 @@ def on_click(x, y, button, pressed):
   if pressed and button==mouse.Button.left:
     if not grabando:
       print('Empieza grabacion')
-      recfile = recorder.Recorder(channels=1, rate=16000).open('tmp/audio.wav', 'wb')
+      recfile = recorder.Recorder(channels=1, rate=48000).open('tmp/audio.wav', 'wb')
       grabando = True
       recfile.start_recording()
-      #numwav += 1
-
+      
   # Soltar el botón izquierdo para dejar de grabar.
   if not pressed and button==mouse.Button.left:
     if grabando:
       print('Termina grabacion')
       recfile.stop_recording()
       recfile.close()
-    # create transformer
-      #tfm = sox.Transformer()
-      #tfm.convert(samplerate=16000, n_channels=1,bitdepth=16)
-      #tfm.build('tmp/audio.wav', 'tmp/audio_final.wav')
+      resample(16000,'tmp/audio')
       recfile = None
       grabando = False
-      procesar('audio.wav')
-      #os.remove('tmp/audio.wav')
-      #os.remove('tmp/audio_final.wav')
-
-  # Presionar el botón derecho para terminar.
-  if pressed and button==mouse.Button.right:
-    if not grabando:
-      return False
-      #return
+      procesar('audio_final.wav')
+      
+      return
 
 
 def main():
