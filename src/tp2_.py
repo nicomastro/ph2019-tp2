db181dbe5844c2cd70f058c35607f6b85a210841
klascolin <me.nicolas7@gmail.com>
2019-12-08 23:16:09 -0300

    Agrego feature de playlist y mejoras en el control de los estados del programa

--- src/tp2_.py
+++ src/tp2_.py
@@ -2,8 +2,9 @@
 import psutil
 from pynput import mouse
 import recorder
-import signal, os
+import signal, os,time
 import sox   
+
 import pandas as pd
 #import simpleaudio as sa
 #from playsound import playsound
@@ -13,7 +14,7 @@ import pyaudio
 import wave
 import sys
 from multiprocessing import Process
-from threading import Thread
+import threading
 from enum import Enum
 from sox import core
 from sox.core import SoxiError
@@ -21,20 +22,34 @@ from SpeechProcessor import SpeechProcessor
 from subprocess import call
 import alsaaudio
 sp=SpeechProcessor()
+
 recfile = None
 grabando = False
 numwav = 0
 pid = -1
 process=None
 proc=None
+playlist_process = None
+playlist_proc = None
+playlist_pid = -1
 listando = 0
 band = ""
 vol = 70
+playlist = None
+playlist_on = False
+
+# Instantiate PyAudio.
+
+
+
+
 
 class Estados(Enum):
   Escuchando = 0
   Reproduciendo = 1
   Pausado = 2
+  Listando = 3
+  Playlisteando = 4
 
 estado = Estados.Escuchando
 
@@ -62,7 +77,7 @@ def play_wav(wav_filename, chunk_size=1024):
 
     # Instantiate PyAudio.
     p = pyaudio.PyAudio()
-
+    print(p.get_default_output_device_info())
     # Open stream.
     stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
         channels=wf.getnchannels(),
@@ -90,6 +105,25 @@ def resToString(res):
   else:
     return ''
 
+def terminar_reproduccion():
+	global estado, playlist_proc, playlist_process, proc, process
+
+	if estado == Estados.Pausado and playlist_on:
+		playlist_proc.resume()
+	elif estado == Estados.Pausado:
+		proc.resume()
+
+	if process != None:
+		process.terminate()
+		proc = None
+		process = None
+	elif playlist_process != None:
+		playlist_proc.terminate()
+		playlist_proc = None
+		playlist_process = None
+
+	return
+
 def resample(rate,file):
   tfm = sox.Transformer()
   tfm.convert(samplerate=rate, n_channels=1,bitdepth=16)
@@ -106,6 +140,7 @@ def listar():
   global listando, band
 
   df = pd.read_csv('canciones/songs.csv',header = 0)
+
   item = ""
   msg = ""
   if(band != ""):
@@ -127,21 +162,26 @@ def listar():
 
   return msg
  
-def pausar():
-  global estado, Estados, process, proc
-  msg = ""
-  if estado == Estados.Reproduciendo:
-    print("Pausando...")
-    msg = "Pausado"
-    estado = Estados.Pausado
-    if process != None:
-      proc.suspend()
-      
-  else:
-    print("No se puede pausar porque no está reproduciendo...")
-    msg = "No se puede pausar porque no está reproduciendo"
 
-  return msg
+def pausar():
+	global estado, Estados, process, proc, playlist_on
+	msg = ""
+	if estado == Estados.Reproduciendo or estado == Estados.Playlisteando:
+		print("Pausando...")
+		msg = "Pausado"
+		if estado == Estados.Reproduciendo:
+			if process != None:
+				proc.suspend()
+		elif estado == Estados.Playlisteando:
+			playlist_on = True
+			if playlist_process != None:
+				playlist_proc.suspend()
+		estado = Estados.Pausado
+	else:
+		print("No se puede pausar porque no está reproduciendo...")
+		msg = "No se puede pausar porque no está reproduciendo"
+
+	return msg
 
 def controlar_volumen(sentido):
 	m = alsaaudio.Mixer()
@@ -152,6 +192,14 @@ def controlar_volumen(sentido):
 		m.setvolume(vol)
 	return
 
+def proximo():
+	global playlist_proc, playlist_process
+	if playlist_process != None:
+		playlist_process.terminate()
+		playlist_process = None
+
+def anterior():
+	return
 
 def continuar():
   global estado, Estados, process, proc
@@ -159,18 +207,48 @@ def continuar():
   if estado == Estados.Pausado:
     print("Reanudando...")
     msg = "Reanudando"
-    estado = Estados.Reproduciendo
-    if process != None:
-      proc.resume()
-  
+    if(playlist_on):
+        estado = Estados.Playlisteando
+        if playlist_process != None:
+           playlist_proc.resume()
+    else:
+        estado = Estados.Reproduciendo
+        if process != None:
+           proc.resume()
   else:
     print("No se puede reanudar porque no está pausado...")
     msg = "No se puede reanudar porque no está pausado"
     
   return msg
 
+def reproducir_playlist():
+
+	global playlist, playlist_proc, playlist_process, playlist_pid, estado 
+
+	for song in playlist:
+
+		playlist_process = Process(target=play_wav, args=('canciones/'+song, 1024))
+		print(playlist_process)
+		
+		playlist_process.start()
+		playlist_pid = playlist_process.pid
+		playlist_proc = psutil.Process(playlist_pid)
+		playlist_process.join()
+		if estado == Estados.Reproduciendo:
+			return
+
+		print("aqui")
+
+	msg = "No hay más canciones"
+	playlist_proc = None
+	playlist_process = None
+	print(msg)
+	#sintetizar
+	estado = Estados.Escuchando
+	return
+
 def procesar(audioName = 'audio_final.wav'):
-	global pid, estado, Estados, process, proc, listando, band,vol
+	global pid, estado, Estados, process, proc, listando, band,vol,playlist
 
 	msg = ""
 	song=""
@@ -193,13 +271,25 @@ def procesar(audioName = 'audio_final.wav'):
 				banda = match.group(0)
 				msg += banda
 				song = any_song(banda) 
+
 			else:
 				encontro = False
-				msg="Especifique banda"
+				msg="No encontré la banda"
+
+		elif(re.search("playlist",str1) and not estado == Estados.Playlisteando):
+
+			terminar_reproduccion()
+
+			if playlist != None and len(playlist) > 0:
+				estado = Estados.Playlisteando
+				t = threading.Thread(target=reproducir_playlist)
+				t.start()
+			else:
+				msg = "No existe ninguna playlist"
 				
 		else:
 			msg = "Reproduciendo "
-			match = re.search("la bestia pop|el revelde|you give love a bad name",str1)
+			match = re.search("la bestia pop|el revelde|you give love a bad name|under pressure",str1)
 			listando = 0
 			if(match):
 				encontro = True
@@ -208,7 +298,7 @@ def procesar(audioName = 'audio_final.wav'):
 				msg += match.group(0)
 				song = re.sub(r" (\w+?)", r"_\1", match.group(0)) + ".wav"		
 			else:
-				msg = "Lo sentimos, nuestro sistema se encuentra en construcción. Pruebe otra canción o listar"
+				msg = "Lo siento, no pude encontrar lo que buscás"
 				encontro = False
 
 	elif re.search("detener|pausar", str1):
@@ -217,24 +307,65 @@ def procesar(audioName = 'audio_final.wav'):
 	elif (re.search("continuar|reanudar",str1)):
 		continuar()
 
+	elif(re.search("próxima|siguiente",str1) and estado == Estados.Playlisteando):
+		proximo()
+
+	elif(re.search("anterior|previa",str1) and estado == Estados.Playlisteando):
+		anterior()
+
 	elif( re.search("listar|listá",str1)):
 
-		if (estado == Estados.Reproduciendo):
-			#pausar()
-			if process != None:
-				proc.terminate()
+		terminar_reproduccion()
+
+		estado = Estados.Escuchando
 
 		match = re.search("queen|la renga|los redondos|bon jovi|épica",str1)
 		if(match):
 			band = match.group(0)
 		msg = listar()
 
+	elif(re.search("armar playlist|crear playlist",str1)):
+
+		terminar_reproduccion()
+
+		estado = Estados.Escuchando
+
+		if(playlist == None):
+			msg = "Comencemos. Diga agregar y el nombre de la primer cancion a agregar"
+		else:
+			msg = "Ya existe una playlist, será reemplazada. Diga agregar y el nombre de la primer cancion"
+		playlist = []
+
+	elif(re.search("agregar",str1) and not playlist == None):
+
+		terminar_reproduccion()
+
+		estado = Estados.Escuchando
+		match = re.search("la bestia pop|el revelde|you give love a bad name|under pressure",str1)
+
+		if (match):	
+			
+			if(playlist == []):
+				msg = "Muy bien. Diga solamente el nombre de la próxima canción o bien terminar"
+			else:
+				msg = "Okay. ¿Próxima canción?"
+			song = re.sub(r" (\w+?)", r"_\1", match.group(0)) + ".wav"	
+			playlist.append(song)
+
+		else:
+			msg = "Lo siento. No encuentro esa canción."
+
+	elif(re.search("terminar",str1) and estado == Estados.Escuchando and not playlist == []):
+		msg = "Cuando quiera, puede reproducir su playlist."
+
 	elif(re.search("si",str1) and listando):
 		msg = listar()
 
 	elif(re.search("no",str1) and listando):
+
 		band = ""
 		listando = 0
+		Estados.Escuchando
 		msg = "¡Entendido!"
 
 	elif(re.search("aumentar volumen|subir volumen|subir",str1)):
@@ -247,20 +378,14 @@ def procesar(audioName = 'audio_final.wav'):
 		print("No reconozco el comando...")
 		msg = "No reconozco el comando. Pruebe listar, y luego, reproducir alguna de la lista."
 
-	if len(msg) > 0:
-		sp.sintetizar('tmp/salida.wav',msg)
-		resample(48000,'tmp/salida')  
-		play_wav('tmp/salida_final.wav')
+	#if len(msg) > 0 and not (estado == Estados.Reproduciendo or estado == Estados.Playlisteando):
+		#sp.sintetizar('tmp/salida.wav',msg)
+		#resample(48000,'tmp/salida')  
+		#play_wav('tmp/salida_final.wav')
 		
 	if encontro == True:
-
-		if process != None:
-			if estdo == Estados.Pausado:
-				proc.resume()
-			proc.terminate()
-
+		terminar_reproduccion()
 		if len(song) > 0:
-
 		    process = Process(target=play_wav, args=('canciones/'+song, 1024))
 		    process.start()
 		    pid = process.pid
@@ -291,12 +416,12 @@ def on_click(x, y, button, pressed):
 		return
 
 def main():
-    print("Presionar el botón izquierdo para empezar a grabar \n. Soltar el botón izquierdo para dejar de grabar.\n Presionar el botón derecho para terminar.")
-    m = alsaaudio.Mixer()
-    m.setvolume(vol)
-    print(vol)
-    with mouse.Listener(on_click=on_click) as listener:
-        listener.join()
+	print("Presionar el botón izquierdo para empezar a grabar \n. Soltar el botón izquierdo para dejar de grabar.\n Presionar el botón derecho para terminar.")
+	m = alsaaudio.Mixer()
+	m.setvolume(vol)
+	print(vol)
+	with mouse.Listener(on_click=on_click) as listener:
+	    listener.join()
 
 if __name__== '__main__':
     main()
